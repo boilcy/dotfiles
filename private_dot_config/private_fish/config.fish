fd3f1161acf036ba2f8660483f3c9a723c52ead1
boilcy <0x6c6379@gmail.com>
2024-07-09 20:53:12 +0800

    modify config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,7 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Created by `pipx` on 2024-07-08 05:11:06
+# seperated files
+source ~/.config/fish/path.fish
+source ~/.config/fish/aliases.fish
+source ~/.config/fish/functions.fish
+# local file for individual machine
+source ~/.config/fish/extra.fish
+
+# pipx
 set PATH $PATH /home/yc/.local/bin
 
-abbr --add config git --git-dir=$HOME/.cfg/ --work-tree=$HOME
+# config management by git bare repo
+# abbr --add config git --git-dir=$HOME/.cfg/ --work-tree=$HOME
